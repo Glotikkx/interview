091766aa1db8f90f66a1304324a690fdb5934aaa
Glotikkx <chenyuwang21@gmail.com>
2025-04-04 11:18:31 -0400

    env

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,15 +3,14 @@ import {getAuth} from "firebase/auth";
 import {getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG8hjGemExLv9yNlD5CFLUsqaTBavwMuI",
-  authDomain: "crocodilo-95b86.firebaseapp.com",
-  projectId: "crocodilo-95b86",
-  storageBucket: "crocodilo-95b86.firebasestorage.app",
-  messagingSenderId: "84684014007",
-  appId: "1:84684014007:web:c8ef5fb740fd6acea86832",
-  measurementId: "G-P1Y9541GF5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 
 
 // Initialize Firebase
